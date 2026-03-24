7e7be1ad10a68b879e09776ad2a063525e29a394
Mazen Naji <mazen.naji.1@gmail.com>
2026-03-24 11:40:31 +0200

    First Commit

---
+++ src/main/java/org/example/aiintelligenceplatformjavaml/AiIntelligencePlatformJavaMlApplication.java
@@ -0,0 +1,13 @@
+package org.example.aiintelligenceplatformjavaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiIntelligencePlatformJavaMlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiIntelligencePlatformJavaMlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/aiintelligenceplatformjavaml/AiIntelligencePlatformJavaMlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aiintelligenceplatformjavaml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiIntelligencePlatformJavaMlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
